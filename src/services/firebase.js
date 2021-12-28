ca797c9d3a5075fbbcba58ff86ab90c7326c3bb1
Zack Wins <el_hefe@mbp.attlocal.net>
2021-12-28 15:41:39 -0500

    removed firebase creds

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,12 +5,12 @@ import { ref, onUnmounted } from 'vue'
 // import { useStore } from 'vuex'
 
 firebase.initializeApp({
-    apiKey: "AIzaSyAdEHnwyImVNHQxQWJ57rsyYnTQhwuMAmQ",
-    authDomain: "crud-app-92a33.firebaseapp.com",
-    projectId: "crud-app-92a33",
-    storageBucket: "crud-app-92a33.appspot.com",
-    messagingSenderId: "299287965542",
-    appId: "1:299287965542:web:f773503e58558c17b122a7"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 })
 
 
